35c9c14b5c8dbefa69ca75cb26a15fe932b81ac2
Rob Culclasure <robculclasure@Robs-MacBook-Pro.local>
2017-02-03 15:06:41 -0600

    first commit

--- src/ATM.java
+++ src/ATM.java
@@ -5,11 +5,9 @@ public class ATM {
 
     static Scanner scanner = new Scanner(System.in);
     static Menu menu = new Menu();
-    static User user = new User();
 
 
     public static void main(String[] args) throws Exception {
-
 
         menu.chooseAccount();
         menu.menuOptions();

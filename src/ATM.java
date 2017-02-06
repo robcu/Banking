0b243e732c3abcc154e05235d2227240198b289f
Rob Culclasure <robculclasure@Robs-MacBook-Pro.local>
2017-02-06 01:28:20 -0600

    Created more methods to clarify flow. Removed recursive method calls. Added while loops. Corrected logic.

--- src/ATM.java
+++ src/ATM.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 
 public class ATM {
 
-    static Scanner scanner = new Scanner(System.in);
     static Menu menu = new Menu();
-
 
     public static void main(String[] args) throws Exception {
 
-        menu.chooseAccount();
-        menu.menuOptions();
+        menu.loggedIn = false;
+
+        while(true){
+            while(!menu.loggedIn){
+                menu.greetUser();
+            }
+            while(menu.loggedIn){
+                menu.processAccountMenu();
+            }
+        }
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Created by robculclasure on 2/2/17.
@@ -6,76 +7,115 @@ import java.util.HashMap;
 public class Menu {
 
     String accountName;
-    int menuChoice;
+    int menuChoice; // right now my program loops because each method calls another method. WHILE LOOP
     double balance;
-    HashMap hashmap = new HashMap();
+    boolean loggedIn;
+    HashMap registry = new HashMap();
+    Scanner greetScanner = new Scanner(System.in);
+    int intGreetChoice;
+    Scanner loginScanner = new Scanner(System.in);
+    Scanner createAccountScanner = new Scanner(System.in);
+    Scanner depositScanner = new Scanner(System.in);
+    Scanner withdrawalScanner = new Scanner(System.in);
+    Scanner accountMenuScanner = new Scanner(System.in);
 
-    void chooseAccount() throws Exception{
-        System.out.println("Welcome to Xbank ATM.");
-        System.out.println("Enter your name: ");
-        accountName = ATM.scanner.nextLine();
-        if(hashmap.containsValue(accountName)){
-            System.out.println("Greetings, " + accountName + ".");
+    void greetUser() throws Exception {
+        System.out.println("Welcome to javaBank ATM.");
+        System.out.println("[1] - Login");
+        System.out.println("[2] - Create a new account");
+        intGreetChoice = greetScanner.nextInt();
+        if(intGreetChoice == 1){
+            login();
         }
-        else {
-            System.out.println("Account not found.");
-            System.out.println("Create a new account? [y/n]");
-            String answer = ATM.scanner.nextLine().toLowerCase();
-            if (answer.contentEquals("y")) {
-                hashmap.put(accountName, balance = 0.00);
-                System.out.println("New account created successfully.\n");
-            } else if (answer.contentEquals("n")) {
-                System.exit(0);
-            } else {
-                throw new Exception("Invalid input");
-            }
+        else if(intGreetChoice == 2){
+            createAccount();
         }
     }
 
-    void menuOptions() throws Exception{
-        System.out.println("Options:");
+    void login() throws Exception{
+        System.out.println("Enter your name:");
+        accountName = loginScanner.nextLine();
+        if(registry.containsKey(accountName)){
+            System.out.println("Welcome " + accountName + ".");
+            loggedIn = true;
+        }
+        else{
+            System.out.println("Account not found. If you believe there is an error please visit your local branch.");
+        }
+    }
+
+    void createAccount() throws Exception {
+        System.out.println("Enter your name:");
+        accountName = createAccountScanner.nextLine();
+        registry.put(accountName, balance = 0.00);
+        System.out.println("New account created successfully.\n");
+        loggedIn = true;
+    }
+
+    void processAccountMenu() throws Exception{
+        System.out.println("Account Options:");
         System.out.println("[1] - Check my balance");
         System.out.println("[2] - Deposit funds");
         System.out.println("[3] - Withdraw funds");
         System.out.println("[4] - Close account");
-        System.out.println("[5] - Cancel");
+        System.out.println("[5] - Log out");
 
-        menuChoice = ATM.scanner.nextInt();
-        while(menuChoice != 5) {
-            switch (menuChoice) {
-                case 1:
-                    System.out.println("Your balance is: $" + hashmap.get(accountName));
-                    break;
-                case 2:
-                    System.out.println("Enter amount to deposit:");
-                    double deposit = ATM.scanner.nextDouble();
-                    hashmap.put(accountName, balance + deposit);
-                    balance += deposit;
-                    System.out.println(hashmap);
-                    break;
-                case 3:
-                    System.out.println("Enter amount to withdraw:");
-                    if (ATM.scanner.nextDouble() > balance) {
-                        System.out.println("Insufficient funds.");
-                        menuOptions();
-                    } else {
-                        System.out.println("Please take your money.");
-                    }
-                    break;
-                case 4:
-                    hashmap.remove(accountName);
-                    System.out.println("Account closed.");
-                    chooseAccount();
-                    break;
-                case 5:
-                    System.out.println("Thank you and please come again.");
-                    chooseAccount();
-                    break;
-                default:
-                    System.out.println("Invalid input.");
-            }
-        menuOptions();
+        menuChoice = accountMenuScanner.nextInt();
+
+        switch (menuChoice) {
+            case 1:
+                checkBalance();
+                break;
+            case 2:
+                makeDeposit();
+                break;
+            case 3:
+                makeWithdrawal();
+                break;
+            case 4:
+                deleteAccount();
+                break;
+            case 5:
+                logOut();
+                break;
+            default:
+                System.out.println("Invalid input.");
         }
+    }
 
+    void checkBalance() throws Exception {
+        System.out.println("Your balance is: $" + registry.get(accountName));
+    }
+
+    void makeDeposit() throws Exception {
+        System.out.println("Enter amount to deposit:");
+        double deposit = depositScanner.nextDouble();
+        balance += deposit;
+        registry.put(accountName, balance);
+        System.out.println("Deposit successful.");
+
+    }
+
+    void makeWithdrawal() throws Exception {
+        System.out.println("Enter amount to withdraw:");
+        double withdrawal = withdrawalScanner.nextDouble();
+        if (withdrawal > balance) {
+            System.out.println("Insufficient funds.");
+        } else {
+            balance -= withdrawal;
+            registry.put(accountName, balance);
+            System.out.println("Dispensing $" + withdrawal + ". Please take your money.");
+        }
+    }
+
+    void deleteAccount() throws Exception {
+        registry.remove(accountName);
+        System.out.println("Dispensing balance and closing account. Please take your money.");
+        loggedIn = false;
+    }
+
+    void logOut() throws Exception {
+        System.out.println("Thank you and please come again.");
+        loggedIn = false;
     }
 }
